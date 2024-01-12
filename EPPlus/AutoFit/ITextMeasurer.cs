e55eb076dd1ad794b1a911220b9f096a41246046
Shane32 <shane@acdmail.com>
2024-01-12 18:40:20 -0500

    Support custom text measuring logic for AutoFit on Linux

---
+++ EPPlus/AutoFit/ITextMeasurer.cs
@@ -0,0 +1,15 @@
+﻿using OfficeOpenXml.Style.XmlAccess;
+using System;
+using System.Drawing;
+
+namespace OfficeOpenXml.AutoFit
+{
+    /// <summary>
+    /// An interface to measure text of a specified font, size, and style.
+    /// </summary>
+    public interface ITextMeasurer : IDisposable
+    {
+        /// <inheritdoc cref="System.Drawing.Graphics.MeasureString(string?, Font, int)"/>
+        SizeF MeasureString(string text, ExcelFontXml excelFont, int width);
+    }
+}
---
+++ EPPlus/AutoFit/WindowsFormsTextMeasurer.cs
@@ -0,0 +1,50 @@
+﻿using OfficeOpenXml.Style.XmlAccess;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace OfficeOpenXml.AutoFit
+{
+#if NET6_0_OR_GREATER
+    [System.Runtime.Versioning.SupportedOSPlatform("windows")]
+#endif
+    public class WindowsFormsTextMeasurer : ITextMeasurer
+    {
+        private readonly Bitmap _bitmap;
+        private readonly Graphics _graphics;
+        private readonly Dictionary<ExcelFontXml, Font> _fonts;
+
+        public WindowsFormsTextMeasurer()
+        {
+            _bitmap = new Bitmap(1, 1);
+            _graphics = Graphics.FromImage(_bitmap);
+        }
+
+#if NET6_0_OR_GREATER
+        [System.Runtime.Versioning.SupportedOSPlatform("windows")]
+#endif
+        public SizeF MeasureString(string text, ExcelFontXml excelFont, int width)
+        {
+            if (!_fonts.TryGetValue(excelFont, out var font))
+            {
+                var fs = FontStyle.Regular;
+                if (excelFont.Bold) fs |= FontStyle.Bold;
+                if (excelFont.UnderLine) fs |= FontStyle.Underline;
+                if (excelFont.Italic) fs |= FontStyle.Italic;
+                if (excelFont.Strike) fs |= FontStyle.Strikeout;
+                font = new Font(excelFont.Name, excelFont.Size, fs);
+                _fonts.Add(excelFont, font);
+            }
+            return _graphics.MeasureString(text, font, width, StringFormat.GenericDefault);
+        }
+
+        public void Dispose()
+        {
+            foreach (var font in _fonts.Values)
+            {
+                font.Dispose();
+            }
+            _graphics.Dispose();
+            _bitmap.Dispose();
+        }
+    }
+}
